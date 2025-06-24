f0a9117b39e94b36469a52a380ee62967ee177dd
RuiMSMelo <ruimsmelo@gmail.com>
2025-06-24 17:51:45 +0100

    movement with data array

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ function Box() {
     const ref = useRef()
     const { viewport } = useThree()
 
+    const [data] = useState({
+        x: THREE.MathUtils.randFloatSpread(2),
+        y: THREE.MathUtils.randFloatSpread(viewport.height),
+        z: 0,
+    })
+
     useFrame(() => {
-        ref.current.position.y += 0.01
-        if (ref.current.position.y > viewport.height / 1.5) {
-            ref.current.position.y = -viewport.height / 1.5
+        ref.current.position.set(data.x * viewport.width, (data.y += 0.1), 0)
+        if (data.y > viewport.height / 1.5) {
+            data.y = -viewport.height / 1.5
         }
     })
 
