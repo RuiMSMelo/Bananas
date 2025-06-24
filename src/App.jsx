c8630c9553199827c32183047693cd521914d3f9
RuiMSMelo <ruimsmelo@gmail.com>
2025-06-24 17:29:30 +0100

    base code App and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-    const [count, setCount] = useState(0)
-
-    return <></>
+export default function App() {
+    return 'hello'
 }
-
-export default App
