63e10d870fbcf659f8a06867cc05a01250a43d4c
RuiMSMelo <ruimsmelo@gmail.com>
2025-06-26 13:25:12 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -48,29 +48,40 @@ function Banana({ z }) {
     )
 }
 
-export default function App({ count = 75, depth = 80 }) {
+export default function App({ count = 100, depth = 80 }) {
     return (
-        <Canvas
-            gl={{ alpha: false }}
-            camera={{ near: 0.01, far: 110, fov: 30 }}
-        >
-            <color attach='background' args={['#ffbf40']} />
-            {/* <ambientLight intensity={0.2} /> */}
-            <spotLight position={[10, 10, 10]} intensity={1} />
-            <Suspense fallback={null}>
-                {Array.from({ length: count }, (_, i) => (
-                    <Banana key={i} z={-(i / count) * depth - 20} />
-                ))}
-                <Environment preset='sunset' />
-                <EffectComposer>
-                    <DepthOfField
-                        target={[0, 0, depth / 2]}
-                        focalLength={0.5}
-                        bokehScale={11}
-                        height={700}
-                    />
-                </EffectComposer>
-            </Suspense>
-        </Canvas>
+        <>
+            <div className='text'>
+                <h1>
+                    LANDING <br />
+                    PAGE -
+                </h1>
+                <h2>with React and Threejs -</h2>
+            </div>
+            <img src='./public/WarholBanana-removebg-preview.png' />
+
+            <Canvas
+                gl={{ alpha: false }}
+                camera={{ near: 0.01, far: 110, fov: 30 }}
+            >
+                <color attach='background' args={['#ffbf40']} />
+                {/* <ambientLight intensity={0.2} /> */}
+                <spotLight position={[10, 10, 10]} intensity={1} />
+                <Suspense fallback={null}>
+                    {Array.from({ length: count }, (_, i) => (
+                        <Banana key={i} z={-(i / count) * depth - 20} />
+                    ))}
+                    <Environment preset='sunset' />
+                    <EffectComposer>
+                        <DepthOfField
+                            target={[0, 0, depth]}
+                            focalLength={0.5}
+                            bokehScale={11}
+                            height={700}
+                        />
+                    </EffectComposer>
+                </Suspense>
+            </Canvas>
+        </>
     )
 }
